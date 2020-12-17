68788f0c1e3354450d64be1dbe0b73e87ac5cc37
Kirill <balanovichks@gmail.com>
2020-12-17 14:49:04 +0300

    style add to header

--- lftaxi/src/components/app-header/app-header.js
+++ lftaxi/src/components/app-header/app-header.js
@@ -3,14 +3,30 @@ import './app-header.css';
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    appBar: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: '0 24px',
+      zIndex: 10,
+      backgroundColor: '#ffffff',
+    },
+  });
 
 const AppHeader = () => {
+    const classes = useStyles();
     return (
-        <div className="app-header">
-            <AppBar className="appBar" color="primary" position="static">
+        <div className="app-header" >
+            <AppBar className={classes.appBar} position="static">
                 <Toolbar>
-                    <Button color="inherit">Login</Button>
+                <Typography variant='h5' color='default' className={classes.grow}>Loft Taxi</Typography>
+                    <Button >Карта</Button>
+                    <Button >Профиль</Button>
+                    <Button >Выйти</Button>
                 </Toolbar>
             </AppBar>
         </div>
